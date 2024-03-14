2861f7a95b79e9d82b8eed91d216baa5373d61e7
shivaboorgula <kumarshiva476@gmail.com>
2024-03-14 00:04:29 -0500

    Added FormGroup And FormControl

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 
 export const routes: Routes = [
-    {path:"", component:NavComponent},
+    {path:"nav", component:NavComponent},
     {path:"home", component:HomeComponent}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,16 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent implements OnInit{
   
   appTitle: string = "";
+
+  applyForm = new FormGroup({
+    storeName: new FormControl(''),
+    storeAddress: new FormControl(''),
+    storePhone: new FormControl(''),
+    storeEmail: new FormControl(''),
+    storeZipcode: new FormControl(''),
+    storeBranch: new FormControl('')
+  });
+  
 
   constructor(){
 
@@ -21,10 +32,22 @@ export class HomeComponent implements OnInit{
 
   
   saveStoreDetails(){
-    if(this.appTitle == null || this.appTitle == '' || this.appTitle == undefined){
-      console.log('appTitle cannot be empty');
-      return;
+    if(this.applyForm.value.storeName == null ||
+       this.applyForm.value.storeName == '' ||
+       this.applyForm.value.storeName == undefined){
+      console.log('store name cannot be empty');
     }
+    
+   if(this.applyForm.value.storeAddress == null ||
+    this.applyForm.value.storeAddress == '' ||
+    this.applyForm.value.storeAddress == undefined){
+   console.log('store address cannot be empty');
+ }
+ if(this.applyForm.value.storePhone == null ||
+  this.applyForm.value.storePhone == '' ||
+  this.applyForm.value.storePhone == undefined){
+ console.log('store phone cannot be empty');
+}
     console.log('hello world')
   }
 
