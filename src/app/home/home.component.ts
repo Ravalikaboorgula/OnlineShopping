2c79dee5ddafc72b29315c37cbec451de7462c3b
shivaboorgula <kumarshiva476@gmail.com>
2025-08-05 15:39:47 -0500

    Moved register store logic to Dialog

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { StoresManagerService } from '../store-manager.service';
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { Store } from '../Store';
+import { Dialog } from 'primeng/dialog';
 
 @Component({
   selector: 'app-home',
-  imports: [ReactiveFormsModule, TableModule, ButtonModule],
+  imports: [ReactiveFormsModule, TableModule, ButtonModule, CommonModule, Dialog],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   providers: [StoresManagerService]
@@ -16,25 +18,27 @@ export class HomeComponent implements OnInit {
   service = inject(StoresManagerService);
   appTitle: string = "";
   appRunning: any = [];
-  displayedColumns = ['storeId', 'streetName', 'city', 'state', 'zipCode', 'email', 'branch', 'storeHours'];
+  displayedColumns = ['storeName', 'streetName', 'city', 'state', 'zipCode', 'email', 'branch', 'storeHours'];
 
   applyForm = new FormGroup({
-    storeName: new FormControl(''),
-    streetName: new FormControl(''),
-    storePhone: new FormControl(''),
-    city: new FormControl(''),
-    state: new FormControl(''),
-    zipCode: new FormControl(''),
+    storeName: new FormControl('', Validators.required),
+    streetName: new FormControl('', Validators.required),
+    city: new FormControl('', Validators.required),
+    state: new FormControl('', Validators.required),
+    zipcode: new FormControl('', Validators.required),
     email: new FormControl(''),
     branch: new FormControl(''),
     storeHours: new FormControl('')
   });
   dataSource: any;
+  formSubmitted: boolean = false;
+  visible: boolean = false;
 
   constructor() {
 
   }
 
+  //This method gets called when the page loads
   ngOnInit() {
     this.getAllStore();
   }
@@ -58,18 +62,27 @@ export class HomeComponent implements OnInit {
   }
 
   saveStoreDetails() {
+    if(this.applyForm.invalid){
+      this.formSubmitted = true;
+      return;
+    }
     let saveStore: Store;
     let saveData = this.applyForm.value;
 
     saveStore = new Store(saveData.storeName!, saveData.streetName!, saveData.city!
-      , saveData.state!, saveData.zipCode!, saveData.email!, saveData.branch!, saveData.storeHours!);
+      , saveData.state!, saveData.zipcode!, saveData.email!, saveData.branch!, saveData.storeHours!);
     this.appRunning = this.service.saveStore(saveStore).subscribe({
 
       next: (response: any) => {
         this.dataSource = response;
+        this.visible = false;
       }
     });
     this.getAllStore();
+  }
+
+  showDialog(){
+    this.visible = true;
   }
 
 }
